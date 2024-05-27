2e4ef4808c198697df88bc0e11c5eb990545fe06
VernSG <68459009+VernSG@users.noreply.github.com>
2024-05-28 04:30:51 +0800

    fix Contact.jsx

--- src/components/pagesHome/Contact.jsx
+++ src/components/pagesHome/Contact.jsx
@@ -1,48 +1,50 @@
 import { IoSend } from "react-icons/io5";
 import { useForm } from "react-hook-form";
-import { db } from "/src/config/firebase.js";
+import { db } from "../../config/firebase";
 import { collection, addDoc } from "firebase/firestore";
 import toast, { Toaster } from "react-hot-toast";
 import { useState } from "react";
-
-const SendMessage = () => {
-  const [loading, setLoading] = useState(false);
-  const userCollectionRef = collection(db, "contactss");
+const Contact = () => {
+  const [loading, setIsLoading] = useState(false);
+  const userCollectionRef = collection(db, "contacts");
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset,
   } = useForm({
     defaultValues: {
       firstName: "",
+      lastName: "",
       email: "",
-      subject: "",
       message: "",
     },
   });
-
   const onSubmit = async (data) => {
-    setLoading(true);
+    setIsLoading(true);
     try {
       await addDoc(userCollectionRef, data);
       toast.success("Successfully Sent");
-      reset();
+      setIsLoading(false);
     } catch (err) {
-      toast.error("Failed to send message. Please try again later.");
+      toast.error(err?.message);
     } finally {
-      setLoading(false);
+      setIsLoading(false);
     }
   };
-
   return (
     <>
-      <section id="contact" className="mt-28">
-        <div className="grid justify-center" data-aos="fade-up" data-aos-offset="50">
-          <h1 className="text-white font-poppins text-3xl sm:text-4xl font-bold uppercase">
-            Send Messages
+      <section id="contact" className="mt-28 ">
+        <div
+          className="grid justify-center"
+          data-aos="fade-up"
+          data-aos-offset="50"
+        >
+          <h1 className="text-white  font-poppins text-3xl sm:text-4xl font-bold uppercase ">
+            Contact Me
           </h1>
-          <p className="uppercase text-pink-400 text-center tracking-widest">Connect with us</p>
+          <p className="uppercase text-pink-400 text-center tracking-widest">
+            Connect with me
+          </p>
         </div>
         <div
           className="boxContainer mt-5"
@@ -55,27 +57,45 @@ const SendMessage = () => {
             noValidate
             className="md:w-2/3 w-full m-auto flex flex-col gap-5"
           >
-            <div className="flex flex-col">
+            <div className="firstname flex flex-col">
               <label
                 htmlFor="firstName"
                 className="text-white font-poppins text-lg tracking-wider font-semibold"
               >
-                Full Name
+                First Name
               </label>
               <input
                 type="text"
-                placeholder="Enter your Full Name"
+                placeholder="Enter your First Name"
                 id="firstName"
                 className="py-3 md:py-3 px-4 mt-3 rounded-md bg-[#1b1b1b] outline-none border-none ring-1 focus:ring-2 font-poppins text-white placeholder:text-slate-300 ring-pink-500"
                 {...register("firstName", {
-                  required: "Full Name is required",
+                  required: "FirstName is required",
                 })}
               />
               <p className="text-red-500 font-karla mt-2">
                 {errors?.firstName?.message}
               </p>
             </div>
-            <div className="flex flex-col">
+            <div className="firstname flex flex-col">
+              <label
+                htmlFor="lastName"
+                className="text-white font-poppins text-lg tracking-wider font-semibold"
+              >
+                Last Name
+              </label>
+              <input
+                type="text"
+                placeholder="Enter your Last Name"
+                id="lastName"
+                className="py-3 md:py-3 px-4 mt-3 rounded-md bg-[#1b1b1b] outline-none border-none ring-1 focus:ring-2 font-poppins text-white placeholder:text-slate-300 ring-pink-500"
+                {...register("lastName", { required: "LastName is required" })}
+              />
+              <p className="text-red-500 font-karla mt-2">
+                {errors?.lastName?.message}
+              </p>
+            </div>
+            <div className="firstname flex flex-col">
               <label
                 htmlFor="email"
                 className="text-white font-poppins text-lg tracking-wider font-semibold"
@@ -93,12 +113,12 @@ const SendMessage = () => {
                 {errors?.email?.message}
               </p>
             </div>
-            <div className="flex flex-col">
+            <div className="comment flex flex-col">
               <label
-                htmlFor="message"
+                htmlFor="email"
                 className="text-white font-poppins text-lg tracking-wider font-semibold"
               >
-                Message
+                Comments / Questions
               </label>
               <textarea
                 placeholder="Enter your Message"
@@ -110,15 +130,23 @@ const SendMessage = () => {
                 {errors?.message?.message}
               </p>
             </div>
-            <button
-              className="py-3 px-5 rounded-md w-full flex items-center justify-center gap-3 bg-pink-600 shadow-sm ring-2 ring-pink-600 hover:ring-pink-500"
-              disabled={loading}
-            >
-              <IoSend className="text-white text-xl" />
-              <span className="text-white font-karla text-xl font-bold">
-                {loading ? "Sending..." : "Send"}
-              </span>
-            </button>
+            {loading ? (
+              <button
+                className="py-3 px-5 rounded-md w-full flex items-center justify-center gap-3 bg-pink-600 shadow-sm ring-2 ring-pink-600 hover:ring-pink-500"
+                disabled
+              >
+                <span className="text-white font-karla text-xl font-bold ">
+                  Loading...
+                </span>
+              </button>
+            ) : (
+              <button className="py-3 px-5 rounded-md w-full flex items-center justify-center gap-3 bg-pink-600 shadow-sm ring-2 ring-pink-600 hover:ring-pink-500">
+                <IoSend className="text-white text-xl" />
+                <span className="text-white font-karla text-xl font-bold ">
+                  Send
+                </span>
+              </button>
+            )}
           </form>
         </div>
       </section>
@@ -127,4 +155,4 @@ const SendMessage = () => {
   );
 };
 
-export default SendMessage;
+export default Contact;
