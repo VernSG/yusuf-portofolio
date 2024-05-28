c82ab32303dcc3e075ce5e5664de25247e1d2d4e
VernSG <68459009+VernSG@users.noreply.github.com>
2024-05-28 08:40:12 +0800

    fix Homepage.jsx
    seo fix

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -28,17 +28,17 @@ const Homepage = () => {
   return (
     <>
       <SEO
-        title="Lucky Indra Efendi - Portfolio"
-        description=" Hello, my name is Lucky Indra Efendi. I have 3 years of experience as a Frontend Developer and 1 year as a Backend Developer. I am skilled in using modern technologies such as React JS and Next JS to develop advanced web applications. For backend development, I utilize Express JS and Nest JS to ensure smooth functionality."
-        author="Lucky Indra Efendi"
-        keywords="Lucky Indra Efendi, Portfolio Lucky Indra Efendi, Portfolio, Frontend Developer, Frontend Developer Portfolio,FullStack Web Developer"
+        title="Muhammad Yusuf Saputra - Portfolio"
+        description=" Hello, my name is Muhammad Yusuf Saputra. I have 3 years of experience as a Frontend Developer and 1 year as a Backend Developer. I am skilled in using modern technologies such as React JS and Next JS to develop advanced web applications. For backend development, I utilize Express JS and Nest JS to ensure smooth functionality."
+        author="Muhammad Yusuf Saputra"
+        keywords="Muhammad Yusuf Saputra, Portfolio Muhammad Yusuf Saputra, Portfolio, Frontend Developer, Frontend Developer Portfolio,FullStack Web Developer"
         robots="index, follow"
         googleBot="index, nofollow, noimageindex, max-video-preview:-1, max-image-preview:large, max-snippet:-1"
         OGUrl={import.meta.env.VITE_PUBLIC_URL}
         OGimage="/preview.png"
-        TwTitle="Lucky Indra Efendi - Portfolio"
+        TwTitle="Muhammad Yusuf Saputra - Portfolio"
         TwCard="/preview.png"
-        TwDesc=" Hello, my name is Lucky Indra Efendi. I have 3 years of experience as a Frontend Developer and 1 year as a Backend Developer. I am skilled in using modern technologies such as React JS and Next JS to develop advanced web applications. For backend development, I utilize Express JS and Nest JS to ensure smooth functionality."
+        TwDesc=" Hello, my name is Muhammad Yusuf Saputra. I have 3 years of experience as a Frontend Developer and 1 year as a Backend Developer. I am skilled in using modern technologies such as React JS and Next JS to develop advanced web applications. For backend development, I utilize Express JS and Nest JS to ensure smooth functionality."
       />
       <div className=" mt-32">
         <Hero />
