6be76219868f0128970c92d5378430eae0144408
VernSG <68459009+VernSG@users.noreply.github.com>
2024-05-28 08:51:29 +0800

    credential

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKpZx1Vf87z10-g6is-o3oXRrZMLnNHb0",
-  authDomain: "yusuf-saputra-8bdd9.firebaseapp.com",
-  projectId: "yusuf-saputra-8bdd9",
-  storageBucket: "yusuf-saputra-8bdd9.appspot.com",
-  messagingSenderId: "813280030714",
-  appId: "1:813280030714:web:69240f00a56657e7bf3e43",
-  measurementId: "G-GXZED8SZME",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
